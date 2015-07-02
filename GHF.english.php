a39bbc323135afb4b67c86c9c22f993db60a9c65
Burke Knight <BurkeKnight@users.noreply.github.com>
2015-07-02 06:56:49 -0400

    Create GHF.english.php

---
+++ GHF.english.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * @package Global Header and Footer ElkArte
+ *
+ * @author BurkeKnight
+ * @copyright 2015 BurkeKnight Enterprises
+ * @license BSD 3-clause
+ * @version 2.0.1
+ *
+ */
+
+$txt['global_head'] = 'Add content to the <head></head> tags:';
+$txt['global_mid'] = 'Add content below linktree:';
+$txt['global_foot'] = 'Add content to the footer:';
+$txt['global_copy'] = 'Add custom copyright to the footer:';
+
+?>
